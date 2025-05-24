0bd19f162ab6ae3f3ee0a53a702ee0ad1afce807
mottocaat <155896816+mottocaat@users.noreply.github.com>
2025-05-24 17:17:29 +0700

    Ignore .terraform, modules, and state files

--- modules/database/output.tf
+++
@@ -1,8 +0,0 @@
-output "config" {
-  value = {
-    user     = aws_db_instance.database.username
-    password = aws_db_instance.database.password
-    hostname = aws_db_instance.database.address
-    port     = aws_db_instance.database.port
-  }
-}
