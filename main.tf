0fe6dd881ada54ec27d651c7027b523050d6f186
mottocaat <155896816+mottocaat@users.noreply.github.com>
2025-05-24 11:55:44 +0700

    first demo

---
+++ main.tf
@@ -0,0 +1,30 @@
+locals {
+  project = "Nemi_Infra"
+}
+
+provider "aws" {
+  region = "ap-southeast-1"
+}
+
+module "networking" {
+  source = "./modules/networking"
+
+  project          = local.project
+  vpc_cidr         = "10.0.0.0/16"
+  private_subnets  = ["10.0.1.0/24"]
+  public_subnets   = ["10.0.4.0/24"]
+  database_subnets = ["10.0.7.0/24"]
+}
+
+module "database" {
+  source = "./modules/database"
+
+  project = local.project
+  vpc     = module.networking.vpc
+  sg      = module.networking.sg
+}
+
+module "autoscaling" {
+  source = "./modules/autoscaling"
+}
+
